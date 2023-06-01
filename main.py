4543c7480e01561e3059d03e3c0f5b89ff306600
00014131id <134965422+00014131id@users.noreply.github.com>
2023-06-01 16:24:26 +0500

    Update main.py
    Begining part

--- main.py
+++ main.py
@@ -1,1 +1,59 @@
+from tkinter import *
+import numpy as np
+import random
+
+size_of_board = 600
+symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
+symbol_thickness = 50
+symbol_X_color = '#EE4035'
+symbol_O_color = '#0492CF'
+Green_color = '#7BC043'
+
+class Tic_Tac_Toe:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Tic-Tac-Toe')
+        self.canvas = Canvas(self.window, width=size_of_board, height=size_of_board)
+        self.canvas.pack()
+        self.window.bind('<Button-1>', self.click)
+
+        self.initialize_board()
+        self.player_X_turns = True
+        self.board_status = np.zeros(shape=(3, 3))
+        self.X_score = self.O_score = self.tie_score = 0
+        self.reset_board = False
+
+    def mainloop(self):
+        self.window.mainloop()
+
+    def initialize_board(self):
+        for i in range(2):
+            self.canvas.create_line((i + 1) * size_of_board / 3, 0, (i + 1) * size_of_board / 3, size_of_board)
+
+        for i in range(2):
+            self.canvas.create_line(0, (i + 1) * size_of_board / 3, size_of_board, (i + 1) * size_of_board / 3)
+
+    def draw_X(self, logical_position):
+        grid_position = self.convert_logical_to_grid_position(logical_position)
+        self.canvas.create_line(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
+                                grid_position[0] + symbol_size, grid_position[1] + symbol_size,
+                                width=symbol_thickness, fill=symbol_X_color)
+        self.canvas.create_line(grid_position[0] - symbol_size, grid_position[1] + symbol_size,
+                                grid_position[0] + symbol_size, grid_position[1] - symbol_size,
+                                width=symbol_thickness, fill=symbol_X_color)
+
+    def draw_O(self, logical_position):
+        grid_position = self.convert_logical_to_grid_position(logical_position)
+        self.canvas.create_oval(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
+                                grid_position[0] + symbol_size, grid_position[1] + symbol_size,
+                                width=symbol_thickness, outline=symbol_O_color)
+
+    def convert_logical_to_grid_position(self, logical_position):
+        grid_position = [(i + 0.5) * size_of_board / 3 for i in logical_position]
+        return grid_position
+
+    def convert_grid_to_logical_position(self, grid_position):
+        logical_position = [int(grid_position[0] / (size_of_board / 3)),
+                            int(grid_position[1] / (size_of_board / 3))]
+        return logical_position
 
